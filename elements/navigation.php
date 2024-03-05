35bae8ff5fe303d5c67afb3e2d34b901a8abe15e
Heimfisch <chris@christian-heinisch.de>
2024-03-05 20:53:42 +0100

    Save Data for Page

--- elements/navigation.php
+++ elements/navigation.php
@@ -1,16 +1,31 @@
             <nav class="uk-navbar-container uk-navbar-transparent">
                 <div>
-                    <div uk-navbar>
+                    <div uk-navbar>            
                         <div class="uk-navbar-left">
-                            <a class="uk-navbar-toggle" href="#">
-                                <span uk-navbar-toggle-icon></span> <span class="uk-margin-small-left">Menu</span>
-                            </a>
-                            <div class="uk-navbar-dropdown">
-                                <ul class="uk-nav uk-navbar-dropdown-nav">
-                                    <li><a href="index.html">Blog</a></li>
-                                    <li><a href="article.html">Article</a></li>
-                                    <li class="uk-active"><a href="page.html">Page</a></li>
-                                </ul>
+                            <a class="uk-navbar-item uk-logo" href="#" aria-label="Back to Home">@{ sitename }</a>
+                        </div>
+                        <div class="uk-navbar-right">
+                            <ul class="uk-navbar-nav">
+                                <li>
+                                    <a><span uk-icon="rss"></span></a>
+                                </li>
+                                <li><a uk-toggle="target: #offcanvas-nav" href="#"><span uk-navbar-toggle-icon></span></a></li>
+                            </ul>
+                            <div id="offcanvas-nav" uk-offcanvas="mode: slide overlay: true">
+                                <div class="uk-offcanvas-bar">
+                                    <ul class="uk-nav uk-nav-default">
+                                        <li class="uk-nav-header">Menu</li>
+                                        <li class="uk-nav-divider"></li>
+                                        <@ newPagelist { type: false, excludeHidden: false } @>
+                                        <@ foreach in pagelist @>
+                                                <@ if @{ :current } @>
+                                                    <li class="uk-active"><a href="@{ url }">@{ title }</a></li>
+                                                <@ else @>
+                                                    <li><a href="@{ url }">@{ title }</a></li>
+                                                <@ end @>
+                                        <@ end @>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>

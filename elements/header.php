6d9f8c6b1a86383218ed7b9f120040583e5c88e9
Heimfisch <chris@christian-heinisch.de>
2024-03-06 10:15:46 +0100

    Update header.php

--- elements/header.php
+++ elements/header.php
@@ -4,21 +4,18 @@
         <meta charset="utf-8" />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="language" content="de">
-        <meta name="description" content="Willkommen auf Heinisch-Design.de. Mein Name ist Christian Heinisch und ich bin nebenberuflicher Webdesigner.">
-        <meta name="keywords" content="Webdesign, Rheingau, Hessen, Rhein-Main, Frankfurt, Wiesbaden, Geisenheim, Wordpress, Bootstrap, Foundation">
-        <meta name="title" content="HeinischDesign">
-        <link rel="shortcut icon" href="/HeinischDesign_Favicon.ico">
-
-        <title>@{ sitename } // @{ title }</title>
-
         <@ Automad/MetaTags {
             description: @{ textTeaser | stripTags },
             ogTitle: @{ metaTitle | stripTags },
             ogDescription: @{ textTeaser | stripTags },
+            ogType: 'website',
             ogImage: @{ ogImage },
             twitterCard: 'summary_large_image'
         } @>
+
+        <title>@{ sitename } // @{ title }</title>
+
+        
 
         <!-- CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.19.1/dist/css/uikit.min.css" />

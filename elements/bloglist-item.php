665ad35265bd1f7dd3b1ccf29da859d8a4b0321a
Christian Heinisch <chris@christian-heinisch.de>
2024-03-11 08:03:56 +0100

    Update bloglist-item.php
    Add Dummy Image

--- elements/bloglist-item.php
+++ elements/bloglist-item.php
@@ -3,7 +3,7 @@
                                 <div class="uk-card">
                                     <div class="uk-card-media-top">
                                         <@ header-image.php @>
-                                        <img src="@{ :imageTeaser }" width="1280" height="720" alt="">
+                                        <img src="https://dummyimage.com/1280x720/ababab/fff.png" width="1280" height="720" alt="">
                                     </div>
                                     <div class="card-body">
                                         <h2 class="head-card">@{ title }</h2>

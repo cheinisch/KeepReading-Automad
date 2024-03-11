f0c4852a5d4eee2129742850451e4da54e927df4
Christian Heinisch <chris@christian-heinisch.de>
2024-03-11 19:28:46 +0100

    Create page_not_found.php
    Add Page not found

---
+++ page_not_found.php
@@ -0,0 +1,13 @@
+<@ elements/header.php @>
+
+    <body>
+        <header>
+<@ elements/navigation.php @>
+        </header>
+        <main>
+            <content class="page">
+                <h1>404</h1>
+                File not Found
+            </content>
+        </main>
+<@ elements/footer.php @>

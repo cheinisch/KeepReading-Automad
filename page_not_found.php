6ef359fd1438b900bdd518a8f0fe510c0e06bee3
Christian Heinisch <chris@christian-heinisch.de>
2024-03-12 20:03:22 +0100

    Aktualisieren von page_not_found.php
    Change Error Description

--- page_not_found.php
+++ page_not_found.php
@@ -13,7 +13,7 @@ http_response_code(404);
                 <h1>HTTP 404 Error</h1>
                 <h2>File not Found</h2>
                 <p>
-                    Looks like you'be followed a broken link ir entered a URL that doesn't exist on this site.
+                    Looks like you'be followed a broken link or entered a URL that doesn't exist on this site.
                 </p>
                 <p>
                     <a href="/">Back to our site</a>

ca7bc1f6fceeda45a9fbcbdc96f95fb236fe0c15
Christian Heinisch <chris@christian-heinisch.de>
2024-03-15 16:14:43 +0100

    Update lang.en.php

--- lang/lang.en.php
+++ lang/lang.en.php
@@ -5,4 +5,8 @@
     return [
         "readmore" => "Read more",
         "keeponreading" => "Keep on Reading",
+        "error404" => "File not Found",
+        "error404description" => "Looks like you'be followed a broken link or entered a URL that doesn't exist on this site.",
+        "errorback" => "Back to our site",
+        "footertext" => "This site is made with <span uk-icon=\"heart\"></span> and <span uk-icon=\"code\"></span> Automad",
     ];

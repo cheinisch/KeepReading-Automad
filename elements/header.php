0527d3799973060505427a7f90c49e5dea3fba3c
Christian Heinisch <chris@christian-heinisch.de>
2024-03-19 21:36:19 +0100

    Update header.php
    Add MEta DAta

--- elements/header.php
+++ elements/header.php
@@ -1,22 +1,29 @@
+<@~ header-image.php @>
+<?php
+$url =  "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+
+$escaped_url = htmlspecialchars( $url, ENT_QUOTES, 'UTF-8' );
+?>
 <!doctype html>
 <html class="no-js" lang="de">
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <@ Automad/MetaTags {
-            ogTitle: @{ metaTitle | stripTags },
-            ogType: 'website',
-            ogImage: @{ ogImage },
-            twitterCard: 'summary_large_image',
-            ogDescription: @{ +main | findFirstParagraph | 300 },
-            description: @{ +main | findFirstParagraph | 300 }
-        } @>
+        <@ if @{ tags } @>
+        <meta name="keywords" content="<@ foreach in tags 	@><@ if @{ :i } > 1 @>, @{ :tag }<@ else @>@{ :tag }<@ end @><@ end @>">
+        <@ end @>
+        <!-- Open Graph -->
+        <meta property="og:title" content="@{ title }" />
+        <meta property="og:type" content="video.movie" />
+        <meta property="og:url" content="<?php echo 'https://'.$escaped_url; ?>" />
+        <@ if @{ :imageCard } ~@>  
+        <meta property="og:image" content="<?php echo 'https://'.$_SERVER['HTTP_HOST']; ?>@{ :imageCard }" />
+        <@~ end ~@>
 
+        <!-- Title -->
         <title>@{ sitename } // @{ title }</title>
-
-        
-
+     
         <!-- CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.19.1/dist/css/uikit.min.css" />
 

f3d73f42db60598e5f861d200ca9fb76838c1a4e
Christian Heinisch <chris@christian-heinisch.de>
2024-03-20 16:07:52 +0100

    Update blog.php

--- blog.php
+++ blog.php
@@ -33,11 +33,12 @@
                 <h1>@{ title }</h1>
 				<@ if @{ ?filter } @>
 					<h2>Filter: @{ ?filter } <a href="@{ url }" uk-icon="icon: close; ratio: 1.5"></a></h2>					
-				<@ end @>
+				<@ else @>
 				<div class="blogtext">
 				@{ +main }
                 @{ text }			
 				</div>
+				<@ end @>
                 <div class="uk-child-width-1-2@s uk-child-width-1-3@m" uk-grid>
                     <@ foreach in pagelist @>
                     <@ elements/bloglist-item.php @>    
